9ebecdb1b95b87c9a919fed7e06ce7fb58e8451f
badbrainz <wormboy.d@gmail.com>
2018-09-23 02:29:45 -0700

    Update on-open-editor.js

--- src/bg/on-open-editor.js
+++ src/bg/on-open-editor.js
@@ -1,11 +1,58 @@
 'use strict';
+/*
+TODO reconnect fileWatchPort if it disconnects unexpectedly.
+*/
+
+(function() {
+
+
+const GREASYHOST_ID = 'greasyhost@geckoid.com'
+
+async function receiveFromGreasyHost(scriptUuid, scriptContent) {
+  const userScript = UserScriptRegistry.scriptByUuid(scriptUuid);
+
+  const downloader = new UserScriptDownloader();
+  downloader.setKnownUuid(scriptUuid);
+  downloader.setScriptUrl(userScript.downloadUrl);
+  downloader.setScriptContent(scriptContent);
+  await downloader.start();
+
+  const [userScriptDetails, downloaderDetails] = await Promise.all([
+    downloader.scriptDetails,
+    downloader.details()
+  ]);
+
+  await onUserScriptInstall({
+    userScript: userScriptDetails,
+    downloader: downloaderDetails
+  });
+}
+
+async function sendToGreasyHost(scriptUuid) {
+  const userScript = UserScriptRegistry.scriptByUuid(scriptUuid);
+  await browser.runtime.sendMessage(GREASYHOST_ID, {
+    'uuid': userScript.uuid,
+    'content': userScript.content
+  })
+}
+
+async function onMessageExternal(message, sender) {
+  if (sender.id !== GREASYHOST_ID) {
+    throw new Error(`Sender not recognized ${sender.id}`);
+  }
+  await receiveFromGreasyHost(message.uuid, message.content);
+}
+browser.runtime.onMessageExternal.addListener(onMessageExternal);
+
+
 function onOpenEditor(message, sender, sendResponse) {
   sendResponse();
-  openNativeApplication(message.uuid)
+  sendToGreasyHost(message.uuid)
     .catch(error => {
-      if (error) {
-        console.warn('Failed to start native application %s', error);
-      }
+      console.warn('greasyhost:', error);
       openUserScriptEditor(message.uuid);
     });
 }
+window.onOpenEditor = onOpenEditor;
+
+})();
